8c272e90248a92e1673e8daf324a381dfbdd2851
Sifat Ibna Amin <sifatibna.amin9@gmail.com>
2020-03-29 18:19:56 +0600

    Debugging urls.py

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 import jobs.views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',jobs.views.home,name='home'),
