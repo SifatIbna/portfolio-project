706f311c5114a6c5747f26cc43d771eec2a5f397
Sifat Ibna Amin <sifatibna.amin9@gmail.com>
2020-04-03 00:13:46 +0600

    changed database info

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER':'postgres',
-        'PASSWORD':'Sifat@5347',
+        'PASSWORD':'', #put your password here
         'HOST':'localhost',
         'PORT':'5432',
     }
